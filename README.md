50f42d0b7fec507f06574ffd4e9d3ad41515e675
Kryptronic <64878608+KryptronicDX@users.noreply.github.com>
2024-09-11 20:47:35 -0500

    Version 0.1.0 Upload
    Initial upload of files

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# FasterStart
-Mod for Factorio
+# Prototype Power Armor
 
-A starter kit with a small modular armor with a small fusion reactor, night vision, exoskeleton, roboport and 25 fusion construction robots.
+Start your mission with a prototype power armor kit.  This kit includes 25 fusion-powered construction robots with the prototype power armor, as well as a prototype fusion reactor, energy shield, exoseleton, and roboport.
+
+A fork and update to the original [FasterStart](https://github.com/magu5026/FasterStart) mod by magu5026.
+
+- Version: 0.1.0
+- Mod Page: https://mods.factorio.com/mod/
+- Factorio Version: 1.1
+- Dependencies
+	- Base >= 1.1.0
--- data.lua
+++ data.lua
@@ -1,79 +1,69 @@
-require("fusion-construction-robot") 
+require("prototypes.fusion-construction-robot") 
 
-data:extend(
-{
-	{
-        type = "equipment-grid",
-		name = "mini-equipment-grid",
-		width = 6,
-		height = 4,
-		equipment_categories = {"armor"}
-	},
-	
-	{
-		type = "item",
-		name = "fusion-construction-robot",
-		icon = "__FasterStart__/graphics/icons/fusion-construction-robot.png",
-		icon_size = 64,
-		icon_mipmaps = 4,
-		subgroup = "logistic-network",
-		order = "a[robot]-b[construction-robot]-a[fusion-construction-robot]",
-		place_result = "fusion-construction-robot",
-		stack_size = 50
-	},
-	
-	{
-		type = "item",
-		name = "mini-fusion-reactor-equipment",
-		icons = 
-		{{
-			icon = "__base__/graphics/icons/fusion-reactor-equipment.png",
-			tint = {r=0.53, g=0.81, b=0.92, a=1}
-		}},		
-		icon_size = 64,
-		icon_mipmaps = 4,
-		placed_as_equipment_result = "mini-fusion-reactor-equipment",
-		subgroup = "equipment",
-		order = "a[energy-source]-b[fusion-reactor]-a[mini-fusion-reactor-equipment]",
-		stack_size = 20
-	},
-	
-	{
-		type = "armor",
-		name = "mini-power-armor",
-		icon = "__FasterStart__/graphics/icons/mini-power-armor.png",
-		icon_size = 64,
-		icon_mipmaps = 4,
-		subgroup = "armor",
-		order = "c[modular-armor]-a[mini-power-armor]",
-		stack_size = 1,
-		infinite = true,
-		equipment_grid = "mini-equipment-grid",
-		inventory_size_bonus = 10
-	},
-	
-	{
-		type = "generator-equipment",
-		name = "mini-fusion-reactor-equipment",
-		sprite =
-		{
-			filename = "__FasterStart__/graphics/equipment/mini-fusion-reactor-equipment.png",
-			width = 65,
-			height = 128,
-			priority = "medium"
-		},
-		shape =
-		{
-			width = 2,
-			height = 4,
-			type = "full"
-		},
-		energy_source =
-		{
-			type = "electric",
-			usage_priority = "primary-output"
-		},
-		power = "250kW",
-		categories = {"armor"}
-	}
+if mods['Krastorio2'] then
+  data.raw['equipment-grid']['mini-equipment-grid'].equipment_categories = {"armor", "universal-equipment", "robot-interaction-equipment"}
+end
+
+data:extend({
+  { type = "equipment-grid",
+    name = "prototype-equipment-grid",
+    width = 6,
+    height = 4,
+    equipment_categories = {"armor"}
+    },
+  
+  { type = "item",
+    name = "fusion-construction-robot",
+    icon = "__prototypearmor__/graphics/icons/fusion-construction-robot.png",
+    icon_size = 64,
+    icon_mipmaps = 4,
+    subgroup = "logistic-network",
+    order = "a[robot]-b[construction-robot]-a[fusion-construction-robot]",
+    place_result = "fusion-construction-robot",
+    stack_size = 50
+    },
+  
+  { type = "item",
+    name = "prototype-fusion-reactor-equipment",
+    icon = "__prototypearmor__/graphics/icons/prototype-fusion-reactor-equipment.png",  
+    icon_size = 64,
+    icon_mipmaps = 4,
+    placed_as_equipment_result = "prototype-fusion-reactor-equipment",
+    subgroup = "equipment",
+    order = "a[energy-source]-b[fusion-reactor]-a[prototype-fusion-reactor-equipment]",
+    stack_size = 20
+    },
+  
+  { type = "armor",
+    name = "prototype-power-armor",
+    icon = "__prototypearmor__/graphics/icons/prototype-power-armor.png",
+    icon_size = 64,
+    icon_mipmaps = 4,
+    subgroup = "armor",
+    order = "c[modular-armor]-a[prototype-power-armor]",
+    stack_size = 1,
+    infinite = true,
+    equipment_grid = "prototype-equipment-grid",
+    inventory_size_bonus = 10,
+    resistances = 
+    { {type = "acid",decrease = 0,percent = 10},
+      {type = "explosion",decrease = 5,percent = 20},
+      {type = "fire",decrease = 0,percent = 10},
+      {type = "physical",decrease = 3,percent = 20}
+      }
+    },
+  
+  { type = "generator-equipment",
+    name = "prototype-fusion-reactor-equipment",
+    sprite =
+    { filename = "__prototypearmor__/graphics/equipment/prototype-fusion-reactor-equipment.png",
+      width = 65,
+      height = 128,
+      priority = "medium"
+      },
+    shape = {width = 2,height = 4,type = "full"},
+    energy_source = {type = "electric",usage_priority = "primary-output"},
+    power = "200kW",
+    categories = {"armor"}
+    }
 })
